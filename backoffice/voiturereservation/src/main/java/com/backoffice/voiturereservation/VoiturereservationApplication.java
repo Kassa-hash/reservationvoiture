eccf788d2e489bb45f9408d68800d0371ca331cd
Kassa-hash <kassa6173@gmail.com>
2026-01-31 09:49:20 +0300

    first commit

---
+++ backoffice/voiturereservation/src/main/java/com/backoffice/voiturereservation/VoiturereservationApplication.java
@@ -0,0 +1,13 @@
+package com.backoffice.voiturereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiturereservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiturereservationApplication.class, args);
+	}
+
+}
---
+++ frontoffice/voiturereservation/src/test/java/com/frontoffice/voiturereservation/VoiturereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontoffice.voiturereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiturereservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
